cb35d0b162b1fd0680ea0214c915fc238441cce1
Abhilash Madhu <50264439+abhilashmadhu24@users.noreply.github.com>
2021-09-08 16:25:00 +0530

    Create ssl_checker.sh

---
+++ ssl_checker.sh
@@ -0,0 +1,38 @@
+#Author Abhilash
+#/bin/bash
+
+figlet -ctk SSL CHECKER
+
+#>domains
+#cat /etc/trueuserdomains | cut -d: -f1 >> domains   #uncomment these two lines, if this is cpanel server
+
+DATE=$(date +"%Y-%m-%d")
+
+> results.txt
+
+echo " SSL status Checked on $DATE " >> results.txt
+
+for i in `cat domains`;
+
+do
+
+echo "--------------------------------------------------------------" >> results.txt
+
+echo "Domain Name:" $i >> results.txt
+
+echo  "SSL expiry on: `echo |openssl s_client -servername $i -connect $i:443 2>/dev/null|openssl x509 -noout -dates|grep 'notAfter'|sed 's/notAfter=//'`" >> results.txt
+
+echo |openssl s_client -servername $i -connect $i:443 2>/dev/null|openssl x509 -noout -issuer|sed 's/issuer=/Issuer: /' > issuer
+
+echo  "SSL Provider: `cat issuer | while IFS="," read -r -a  line; do printf "%s\n" "${line[@]}"; done | grep O | cut -d\  -f4-5`" >> results.txt
+echo ""
+
+rm -rf issuer;
+
+echo "--------------------------------------------------------------" >> results.txt
+
+done
+
+cat results.txt
+
+figlet -ctk THANK YOU !
